1901ff8c316495a775dc2c6cfdc300ba9ab306d9
demijurg666 <44579515+demijurg666@users.noreply.github.com>
2021-11-07 21:47:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,264 @@
+function oboj1() {
+  let prvi = document.getElementById("prvi");
+  prvi.style.background = "red";
+}
+
+function vrati1() {
+  let prvi = document.getElementById("prvi");
+  prvi.style.background = "white";
+}
+
+function oboj2() {
+  let drugi = document.getElementById("drugi");
+  drugi.style.background = "red";
+}
+
+function vrati2() {
+  let drugi = document.getElementById("drugi");
+  drugi.style.background = "white";
+}
+
+function oboj3() {
+  let treci = document.getElementById("treci");
+  treci.style.background = "red";
+}
+
+function vrati3() {
+  let treci = document.getElementById("treci");
+  treci.style.background = "white";
+}
+
+function oboj4() {
+  let cetvrti = document.getElementById("cetvrti");
+  cetvrti.style.background = "red";
+}
+
+function vrati4() {
+  let cetvrti = document.getElementById("cetvrti");
+  cetvrti.style.background = "white";
+}
+
+//tekst izdavalastvo
+function prikazi() {
+  let opisKnjige = document.getElementById("opisKnjige");
+  opisKnjige.style.visibility = "visible";
+}
+
+//slike izdavalastvo
+
+function prvaSl() {
+  let prvaSl = document.getElementById("prvaSl");
+  let drugaSl = document.getElementById("drugaSl");
+  let trecaSl = document.getElementById("trecaSl");
+  let cetvrtaSl = document.getElementById("cetvrtaSl");
+  let petaSl = document.getElementById("petaSl");
+  let sestaSl = document.getElementById("sestaSl");
+
+  prvaSl.style.border = "red 5px solid";
+
+  drugaSl.style.opacity = "0.5";
+  trecaSl.style.opacity = "0.5";
+  cetvrtaSl.style.opacity = "0.5";
+  petaSl.style.opacity = "0.5";
+  sestaSl.style.opacity = "0.5";
+}
+
+function prvaSlOut() {
+  let prvaSl = document.getElementById("prvaSl");
+  let drugaSl = document.getElementById("drugaSl");
+  let trecaSl = document.getElementById("trecaSl");
+  let cetvrtaSl = document.getElementById("cetvrtaSl");
+  let petaSl = document.getElementById("petaSl");
+  let sestaSl = document.getElementById("sestaSl");
+
+  drugaSl.style.opacity = "1";
+  trecaSl.style.opacity = "1";
+  cetvrtaSl.style.opacity = "1";
+  petaSl.style.opacity = "1";
+  sestaSl.style.opacity = "1";
+
+  prvaSl.style.border = "none";
+}
+
+function drugaSl() {
+  let prvaSl = document.getElementById("prvaSl");
+  let drugaSl = document.getElementById("drugaSl");
+  let trecaSl = document.getElementById("trecaSl");
+  let cetvrtaSl = document.getElementById("cetvrtaSl");
+  let petaSl = document.getElementById("petaSl");
+  let sestaSl = document.getElementById("sestaSl");
+
+  drugaSl.style.border = "red 5px solid";
+
+  prvaSl.style.opacity = "0.5";
+  trecaSl.style.opacity = "0.5";
+  cetvrtaSl.style.opacity = "0.5";
+  petaSl.style.opacity = "0.5";
+  sestaSl.style.opacity = "0.5";
+}
+
+function drugaSlOut() {
+  let prvaSl = document.getElementById("prvaSl");
+  let drugaSl = document.getElementById("drugaSl");
+  let trecaSl = document.getElementById("trecaSl");
+  let cetvrtaSl = document.getElementById("cetvrtaSl");
+  let petaSl = document.getElementById("petaSl");
+  let sestaSl = document.getElementById("sestaSl");
+
+  prvaSl.style.opacity = "1";
+  trecaSl.style.opacity = "1";
+  cetvrtaSl.style.opacity = "1";
+  petaSl.style.opacity = "1";
+  sestaSl.style.opacity = "1";
+
+  drugaSl.style.border = "none";
+}
+
+function trecaSl() {
+  let prvaSl = document.getElementById("prvaSl");
+  let drugaSl = document.getElementById("drugaSl");
+  let trecaSl = document.getElementById("trecaSl");
+  let cetvrtaSl = document.getElementById("cetvrtaSl");
+  let petaSl = document.getElementById("petaSl");
+  let sestaSl = document.getElementById("sestaSl");
+
+  trecaSl.style.border = "red 5px solid";
+
+  prvaSl.style.opacity = "0.5";
+  drugaSl.style.opacity = "0.5";
+  cetvrtaSl.style.opacity = "0.5";
+  petaSl.style.opacity = "0.5";
+  sestaSl.style.opacity = "0.5";
+}
+
+function trecaSlOut() {
+  let prvaSl = document.getElementById("prvaSl");
+  let drugaSl = document.getElementById("drugaSl");
+  let trecaSl = document.getElementById("trecaSl");
+  let cetvrtaSl = document.getElementById("cetvrtaSl");
+  let petaSl = document.getElementById("petaSl");
+  let sestaSl = document.getElementById("sestaSl");
+
+  prvaSl.style.opacity = "1";
+  drugaSl.style.opacity = "1";
+  cetvrtaSl.style.opacity = "1";
+  petaSl.style.opacity = "1";
+  sestaSl.style.opacity = "1";
+
+  trecaSl.style.border = "none";
+}
+
+function cetvrtaSl() {
+  let prvaSl = document.getElementById("prvaSl");
+  let drugaSl = document.getElementById("drugaSl");
+  let trecaSl = document.getElementById("trecaSl");
+  let cetvrtaSl = document.getElementById("cetvrtaSl");
+  let petaSl = document.getElementById("petaSl");
+  let sestaSl = document.getElementById("sestaSl");
+
+  cetvrtaSl.style.border = "red 5px solid";
+
+  prvaSl.style.opacity = "0.5";
+  drugaSl.style.opacity = "0.5";
+  trecaSl.style.opacity = "0.5";
+  petaSl.style.opacity = "0.5";
+  sestaSl.style.opacity = "0.5";
+}
+
+function cetvrtaSlOut() {
+  let prvaSl = document.getElementById("prvaSl");
+  let drugaSl = document.getElementById("drugaSl");
+  let trecaSl = document.getElementById("trecaSl");
+  let cetvrtaSl = document.getElementById("cetvrtaSl");
+  let petaSl = document.getElementById("petaSl");
+  let sestaSl = document.getElementById("sestaSl");
+
+  prvaSl.style.opacity = "1";
+  drugaSl.style.opacity = "1";
+  trecaSl.style.opacity = "1";
+  petaSl.style.opacity = "1";
+  sestaSl.style.opacity = "1";
+
+  cetvrtaSl.style.border = "none";
+}
+
+function petaSl() {
+  let prvaSl = document.getElementById("prvaSl");
+  let drugaSl = document.getElementById("drugaSl");
+  let trecaSl = document.getElementById("trecaSl");
+  let cetvrtaSl = document.getElementById("cetvrtaSl");
+  let petaSl = document.getElementById("petaSl");
+  let sestaSl = document.getElementById("sestaSl");
+
+  petaSl.style.border = "red 5px solid";
+
+  prvaSl.style.opacity = "0.5";
+  drugaSl.style.opacity = "0.5";
+  trecaSl.style.opacity = "0.5";
+  cetvrtaSl.style.opacity = "0.5";
+  sestaSl.style.opacity = "0.5";
+}
+
+function petaSlOut() {
+  let prvaSl = document.getElementById("prvaSl");
+  let drugaSl = document.getElementById("drugaSl");
+  let trecaSl = document.getElementById("trecaSl");
+  let cetvrtaSl = document.getElementById("cetvrtaSl");
+  let petaSl = document.getElementById("petaSl");
+  let sestaSl = document.getElementById("sestaSl");
+
+  prvaSl.style.opacity = "1";
+  drugaSl.style.opacity = "1";
+  trecaSl.style.opacity = "1";
+  cetvrtaSl.style.opacity = "1";
+  sestaSl.style.opacity = "1";
+
+  petaSl.style.border = "none";
+}
+
+function sestaSl() {
+  let prvaSl = document.getElementById("prvaSl");
+  let drugaSl = document.getElementById("drugaSl");
+  let trecaSl = document.getElementById("trecaSl");
+  let cetvrtaSl = document.getElementById("cetvrtaSl");
+  let petaSl = document.getElementById("petaSl");
+  let sestaSl = document.getElementById("sestaSl");
+
+  sestaSl.style.border = "red 5px solid";
+
+  prvaSl.style.opacity = "0.5";
+  drugaSl.style.opacity = "0.5";
+  trecaSl.style.opacity = "0.5";
+  cetvrtaSl.style.opacity = "0.5";
+  petaSl.style.opacity = "0.5";
+}
+
+function sestaSlOut() {
+  let prvaSl = document.getElementById("prvaSl");
+  let drugaSl = document.getElementById("drugaSl");
+  let trecaSl = document.getElementById("trecaSl");
+  let cetvrtaSl = document.getElementById("cetvrtaSl");
+  let petaSl = document.getElementById("petaSl");
+  let sestaSl = document.getElementById("sestaSl");
+
+  prvaSl.style.opacity = "1";
+  drugaSl.style.opacity = "1";
+  trecaSl.style.opacity = "1";
+  cetvrtaSl.style.opacity = "1";
+  petaSl.style.opacity = "1";
+
+  sestaSl.style.border = "none";
+}
+
+//email ili telefon
+
+function email() {
+  let text = document.getElementById("text");
+
+  text.innerHTML = "nesa@mlinpekzavod.com";
+}
+
+function tel() {
+  let text = document.getElementById("text");
+  text.innerHTML = "+381(0)63553406";
+}
